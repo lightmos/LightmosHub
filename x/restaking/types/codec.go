2db5cd5bb681150041ff6b077dd36c0e17e41930
Sai <51160269@qq.com>
2023-07-05 15:05:19 +0800

    init repo

---
+++ x/restaking/types/codec.go
@@ -0,0 +1,43 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgSendCreatePair{}, "restaking/SendCreatePair", nil)
+	cdc.RegisterConcrete(&MsgSendSellOrder{}, "restaking/SendSellOrder", nil)
+	cdc.RegisterConcrete(&MsgSendBuyOrder{}, "restaking/SendBuyOrder", nil)
+	cdc.RegisterConcrete(&MsgCancelSellOrder{}, "restaking/CancelSellOrder", nil)
+	cdc.RegisterConcrete(&MsgCancelBuyOrder{}, "restaking/CancelBuyOrder", nil)
+	// this line is used by starport scaffolding # 2
+}
+
+func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgSendCreatePair{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgSendSellOrder{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgSendBuyOrder{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCancelSellOrder{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCancelBuyOrder{},
+	)
+	// this line is used by starport scaffolding # 3
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+var (
+	Amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+)
