0fb7e34a66add406a62c90d10c41dbb47940e1af
mindcarver <626984947@qq.com>
2023-07-05 17:20:27 +0800

    feat:add cli

---
+++ x/restaking/types/restaking_msg.go
@@ -0,0 +1,126 @@
+package types
+
+import (
+	"cosmossdk.io/math"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+var (
+	_ sdk.Msg                            = &MsgCreateValidator{}
+	_ codectypes.UnpackInterfacesMessage = (*MsgCreateValidator)(nil)
+	_ sdk.Msg                            = &MsgCreateValidator{}
+)
+
+// UnpackInterfaces implements UnpackInterfacesMessage.UnpackInterfaces
+func (msg MsgCreateValidator) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+	var pubKey cryptotypes.PubKey
+	return unpacker.UnpackAny(msg.Pubkey, &pubKey)
+}
+
+func NewMsgCreateValidator(
+	valAddr sdk.ValAddress, pubKey cryptotypes.PubKey,
+	selfDelegation sdk.Coin, description Description, commission CommissionRates, minSelfDelegation math.Int,
+) (*MsgCreateValidator, error) {
+	var pkAny *codectypes.Any
+	if pubKey != nil {
+		var err error
+		if pkAny, err = codectypes.NewAnyWithValue(pubKey); err != nil {
+			return nil, err
+		}
+	}
+	selfDelegationConvert := Coin{
+		Denom:  selfDelegation.Denom,
+		Amount: selfDelegation.Amount,
+	}
+	return &MsgCreateValidator{
+		Description:       description,
+		DelegatorAddress:  sdk.AccAddress(valAddr).String(),
+		ValidatorAddress:  valAddr.String(),
+		Pubkey:            pkAny,
+		Value:             selfDelegationConvert,
+		Commission:        commission,
+		MinSelfDelegation: minSelfDelegation,
+	}, nil
+}
+
+// ValidateBasic implements the sdk.Msg interface.
+func (msg MsgCreateValidator) ValidateBasic() error {
+	// note that unmarshaling from bech32 ensures both non-empty and valid
+	delAddr, err := sdk.AccAddressFromBech32(msg.DelegatorAddress)
+	if err != nil {
+		return sdkerrors.ErrInvalidAddress.Wrapf("invalid delegator address: %s", err)
+	}
+	valAddr, err := sdk.ValAddressFromBech32(msg.ValidatorAddress)
+	if err != nil {
+		return sdkerrors.ErrInvalidAddress.Wrapf("invalid validator address: %s", err)
+	}
+	if !sdk.AccAddress(valAddr).Equals(delAddr) {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "validator address is invalid")
+	}
+
+	if msg.Pubkey == nil {
+		return ErrEmptyValidatorPubKey
+	}
+
+	if !msg.Value.IsValid() || !msg.Value.Amount.IsPositive() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid delegation amount")
+	}
+
+	if msg.Description == (Description{}) {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "empty description")
+	}
+
+	if msg.Commission == (CommissionRates{}) {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "empty commission")
+	}
+
+	if err := msg.Commission.Validate(); err != nil {
+		return err
+	}
+
+	if !msg.MinSelfDelegation.IsPositive() {
+		return sdkerrors.Wrap(
+			sdkerrors.ErrInvalidRequest,
+			"minimum self delegation must be a positive integer",
+		)
+	}
+
+	if msg.Value.Amount.LT(msg.MinSelfDelegation) {
+		return ErrSelfDelegationBelowMinimum
+	}
+
+	return nil
+}
+
+func (msg MsgCreateValidator) GetSigners() []sdk.AccAddress {
+	// delegator is first signer so delegator pays fees
+	delegator, _ := sdk.AccAddressFromBech32(msg.DelegatorAddress)
+	addrs := []sdk.AccAddress{delegator}
+	valAddr, _ := sdk.ValAddressFromBech32(msg.ValidatorAddress)
+
+	valAccAddr := sdk.AccAddress(valAddr)
+	if !delegator.Equals(valAccAddr) {
+		addrs = append(addrs, valAccAddr)
+	}
+
+	return addrs
+}
+
+// GetSignBytes returns the message bytes to sign over.
+func (msg MsgCreateValidator) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(&msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func NewDescription(moniker, identity, website, securityContact, details string) Description {
+	return Description{
+		Moniker:         moniker,
+		Identity:        identity,
+		Website:         website,
+		SecurityContact: securityContact,
+		Details:         details,
+	}
+}
